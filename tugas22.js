cb2d83a61668abf3287eea1a18c5b0b307795a2f
as <adbudi1@gmail.com>
2021-07-30 09:16:26 +0700

    tugas 22 javascript
    tugas ini dikumpulkan untuk niomic

---
+++ tugas22.js
@@ -0,0 +1,8 @@
+function panggilforEach() {
+  var kata = ['Saya','ingin', 'belajar','bersama'];
+  kata.forEach(function(item, i)  {
+    console.log('item:',item,'indek ke',i);
+  });
+
+}
+panggilforEach()
